bf7cbe2c9ead338618f48dc8e8a2ab8522de59c1
Vijith Assar <3488572+vijithassar@users.noreply.github.com>
2018-10-24 13:09:52 -0400

    add rollup and configure build process

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+const config = {
+  input: "./index.js",
+  output: {
+    file: "./build/microlens.js",
+    format: "umd",
+    name: "microlens",
+    sourcemap: true
+  }
+}
+
+export default config
